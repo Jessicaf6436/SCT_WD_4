369a1587e02c99b87fc218a1219f2a839ad6fd99
Jessica <jessicaf6436@gmail.com>
2025-09-27 19:13:36 +0530

    inital commit

---
+++ Untitled-1.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/todoapp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Task Schema
+const taskSchema = new mongoose.Schema({
+  description: String,
+  dateTime: String,
+  completed: { type: Boolean, default: false },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+// Routes
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.json(task);
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedTask);
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ success: true });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
